1549818fa8cd8d468bb40ed336f882c4324238c1
Nikimad <91181114+Nikimad@users.noreply.github.com>
2021-12-08 14:40:25 +0300

    Update calcGame.js

--- games/calcGame.js
+++ games/calcGame.js
@@ -13,6 +13,7 @@ export default {
     const [operand1, operand2] = [getRandomNum(0, 100), getRandomNum(0, 100)];
     const randExp = mathematics[getRandomNum(0, 2)];
     const [quest, correctAnswer] = [randExp(operand1, operand2, 'exp'), randExp(operand1, operand2)];
-    return { quest, correctAnswer };
+    return { quest: quest,
+     correctAnswer:correctAnswer, };
   },
 };
