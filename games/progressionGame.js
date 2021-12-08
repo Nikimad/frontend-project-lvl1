03f65cd4f887453f476cd6f4b116e32dd07704aa
Nikimad <91181114+Nikimad@users.noreply.github.com>
2021-12-08 15:14:01 +0300

    Update progressionGame.js

--- games/progressionGame.js
+++ games/progressionGame.js
@@ -9,22 +9,29 @@ const getProgression = (startNum, step, size = 10) => {
 };
 
 const blurElemofProgression = (progression, index) => {
-  progression[index] = '..';
-  return progression.join(' ');
-};
+  const progressionWhithBluredElem = [];
+  for (let i = 0; i < progression.length; i += 1) {
+    if (i === index) {
+      progressionWhithBluredElem.push('..');
+    } else {
+      progressionWhithBluredElem.push(progression[i]);
+    }
+  }
+  return progressionWhithBluredElem.join(' ');
+}
 
 const getElemofProgression = (startNum, index, step) => startNum + index * step;
-//GameModule
-export default {
+
+const progressionGame = {
   rules: 'What number is missing in the progression?',
   validator: (answer) => !Number.isInteger(+answer),
   getQnA: () => {
     const startNum = getRandomNum(0, 20);
     const step = getRandomNum(1, 100);
     const progression = getProgression(startNum, step);
-    const indexOfElem = getRandomNum(0, progression.length)
+    const indexOfElem = getRandomNum(0, progression.length - 1)
     const quest = blurElemofProgression(progression, indexOfElem);
-    const bluredElem = `${getElemofProgression(startNum, indexOfElem, step)}`;
-    return { quest, bluredElem };
+    const correctAnswer= `${getElemofProgression(startNum, indexOfElem, step)}`;
+    return { quest, correctAnswer };
   },
 };
