b1ecd356d01eb94634f9548e83567bdf98ae5976
Nikimad <91181114+Nikimad@users.noreply.github.com>
2021-12-08 14:38:16 +0300

    Create gsdGame.js

---
+++ games/gsdGame.js
@@ -0,0 +1,18 @@
+import getRandomNum from '../src/getRandomNum.js';
+
+const gsd = (x, y) => {
+	if (y > x) return gsd(y, x);
+	if (!y) return x;
+	return gsd(y, x % y);
+};
+
+const gsdGame = {
+  rules: 'Find the greatest common divisor of given numbers.',
+  validator: (answer) => !Number.isInteger(+answer),
+  getQnA: () => {
+  const [num1, num2] = [getRandomNum(0, 100), getRandomNum(0, 100)];
+  const quest = `${num1} ${num2}`;
+  const correctAnswer = `${gsd(num1, num2)}`;
+  return { quest, correctAnswer };
+  },
+};
