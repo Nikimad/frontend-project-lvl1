207664deaaee95c2804918b998243a82b581d7e7
Nikimad <91181114+Nikimad@users.noreply.github.com>
2021-12-08 14:43:37 +0300

    Create primeGame.js

---
+++ games/primeGame.js
@@ -0,0 +1,30 @@
+import getRandomNum from '../src/getRandomNum.js';
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  } else if (num === 2) {
+    return true;
+  }
+  let i = 2;
+  const limit = Math.sqrt(num);
+  while (i <= limit) {
+    if (num % i === 0) {
+      return false;
+    }
+    i += 1;
+  }
+  return true;
+};
+
+export default {
+  rules: 'Answer "yes" if given number is prime. Otherwise answer "no".',
+  validator: (answer) => answer !== 'yes' && answer !== 'no',
+  getQnA: () => {
+  const quest = getRandomNum(1, 100);
+  const correctAnswer = isPrime(quest) ? 'yes' : 'no';
+  return {quest: quest,
+   correctAnswer: correctAnswer,
+   };
+  },
+};
