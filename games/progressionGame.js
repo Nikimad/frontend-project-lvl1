89b50a90a3f8dd1279d7cab4144deaa06888700c
Nikimad <91181114+Nikimad@users.noreply.github.com>
2021-12-08 14:39:41 +0300

    Create progressionGame.js

---
+++ games/progressionGame.js
@@ -0,0 +1,32 @@
+import getRandomNum from '../src/getRandomNum.js';
+
+const getProgression = (startNum, step, size = 10) => {
+  const progression = [startNum];
+  for (let i = 1; i < size; i += 1) {
+    progression[i] = progression[i - 1] + step;
+  }
+  return progression;
+};
+
+const blurElemofProgression = (progression, index) => {
+  progression[index] = '..';
+  return progression.join(' ');
+};
+
+const getElemofProgression = (startNum, index, step) => startNum + index * step;
+//GameModule
+const progressionGame = {
+  rules: 'What number is missing in the progression?',
+  validator: (answer) => !Number.isInteger(+answer),
+  getQnA: () => {
+    const startNum = getRandomNum(0, 20);
+    const step = getRandomNum(1, 100);
+    const progression = getProgression(startNum, step);
+    const indexOfElem = getRandomNum(0, progression.length)
+    const quest = blurElemofProgression(progression, indexOfElem);
+    const bluredElem = `${getElemofProgression(startNum, indexOfElem, step)}`;
+    return {quest: quest,
+     correctAnswer: bluredElem,
+    };
+  },
+};
