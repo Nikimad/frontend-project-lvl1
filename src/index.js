4421cebe3c9b1b3a0416aa9a7cf6d18436da2ce8
Nikimad <91181114+Nikimad@users.noreply.github.com>
2021-12-08 13:36:12 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import messages from './cli.js';
+
+const counter = (name, game, maxAttempts) => {
+  let count = 0;
+  while (count < maxAttempts) {
+    if (game()) {
+      count += 1;
+    } else {
+    return messages.luse(name);
+    };
+  };
+  return messages.win(name);
+};
+
+const checker = (answer, correctAnswer) => {
+  if (correctAnswer === answer) {
+    messages.correct();
+    return true;
+  }
+  messages.wrong(answer, correctAnswer);
+  return false;
+};
+
+const generalScenario = ({rules, getQnA, validator}, maxAttempts) => {
+  messages.welcome();
+  const name = messages.getName();
+  messages.greetings(name);
+  messages.rules(rules);
+  const game = () => {
+  const {quest, correctAnswer} = getQnA();
+  messages.question(quest);
+  const answer = messages.answer();
+  if (validator(answer)) {
+    return false;
+  }
+  return checker(answer, correctAnswer);
+  };
+  counter(name, game, maxAttempts);
+};
