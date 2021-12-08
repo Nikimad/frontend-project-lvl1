32807ca5172e2740a7cc92ee24a878619ad46a2b
Nikimad <91181114+Nikimad@users.noreply.github.com>
2021-12-08 14:50:28 +0300

    Update gsdGame.js

--- games/gsdGame.js
+++ games/gsdGame.js
@@ -1,18 +1,18 @@
 import getRandomNum from '../src/getRandomNum.js';
 
 const gsd = (x, y) => {
-	if (y > x) return gsd(y, x);
-	if (!y) return x;
-	return gsd(y, x % y);
+  if (y > x) return gsd(y, x);
+  if (!y) return x;
+  return gsd(y, x % y);
 };
 
 export default {
   rules: 'Find the greatest common divisor of given numbers.',
   validator: (answer) => !Number.isInteger(+answer),
   getQnA: () => {
-  const [num1, num2] = [getRandomNum(0, 100), getRandomNum(0, 100)];
-  const quest = `${num1} ${num2}`;
-  const correctAnswer = `${gsd(num1, num2)}`;
-  return { quest, correctAnswer };
+    const [num1, num2] = [getRandomNum(0, 100), getRandomNum(0, 100)];
+    const quest = `${num1} ${num2}`;
+    const correctAnswer = `${gsd(num1, num2)}`;
+    return { quest, correctAnswer };
   },
 };
